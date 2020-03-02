16655100b4165a76f6557d0303956ebfb3d3af96
Luis Enrique Suarez Rosas <luuis.e@outlook.com>
2020-03-02 16:31:51 -0600

    #003 - First build

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,31 @@ module.exports = function(grunt) {
 		jshint: {
 			src: ['js/*.js', '!js/*.min.js']
 		},
+		uglify: {
+			options: {
+				compress: {
+					drop_console: true
+				},
+				output: {
+					comments: 'all'
+				}
+			},
+			js: {
+				files: [{
+					cwd: 'js/src/',
+					expand: true,
+					src: '*.js',
+					dest: 'js/'
+				}]
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('minify', ['uglify']);
 
 };
